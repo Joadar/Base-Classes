acc85cf695ae5ec76adb0508282ba9a3da41eae3
Jonathan Pannetier <jonathan.pannetier@gmail.com>
2017-10-10 19:21:10 +0200

    Add ShareUtils

---
+++ library/src/main/java/io/smallant/baseclasses/utils/ShareUtils.kt
@@ -0,0 +1,25 @@
+package io.smallant.baseclasses.utils
+
+import android.content.Context
+import android.content.Intent
+import io.smallant.baseclasses.R
+
+/**
+ * Created by Jonathan on 10/10/2017.
+ */
+object ShareUtils {
+    val intent = Intent()
+    lateinit var context: Context
+
+    fun share(context: Context, value: String) {
+        this.context = context
+        intent.putExtra(Intent.EXTRA_TEXT, value)
+        share()
+    }
+
+    private fun share() {
+        intent.action = Intent.ACTION_SEND
+        intent.type = "text/plain"
+        context.startActivity(Intent.createChooser(intent, context.resources.getText(R.string.share_with)))
+    }
+}
--- sample/src/main/kotlin/io/smallant/baseclasses/sample/features/home/HomeFragment.kt
+++ sample/src/main/kotlin/io/smallant/baseclasses/sample/features/home/HomeFragment.kt
@@ -2,9 +2,9 @@ package io.smallant.baseclasses.sample.features.home
 
 import android.os.Bundle
 import android.view.View
-import io.smallant.baseclasses.extensions.toast
 import io.smallant.baseclasses.sample.R
 import io.smallant.baseclasses.ui.BaseFragmentRecycler
+import io.smallant.baseclasses.utils.ShareUtils
 
 /**
  * Created by Jonathan on 08/10/2017.
@@ -22,6 +22,7 @@ class HomeFragment : BaseFragmentRecycler<HomePresenter, StringsRecyclerAdapter,
     }
 
     override fun onItemClicked(position: Int) {
-        context.toast("${items[position]} clicked!")
+        ShareUtils.share(context, "${items[position]} clicked!")
+        //context.toast("${items[position]} clicked!")
     }
 }
